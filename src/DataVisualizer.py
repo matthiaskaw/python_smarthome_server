41678b1c42a3e743133179c12ed107d03e9ef067
matthiaskaw <Matthias.Kawalek@t-online.de>
2026-02-15 21:21:17 +0100

    removed unnecessary html parts

--- src/DataVisualizer.py
+++ src/DataVisualizer.py
@@ -34,23 +34,8 @@ class DataVisualizer(ILiveData):
         
         self._logger.info("Setting API endpoints")
         self.router.add_api_route("/", self._root, methods=["GET"])
-        self.router.add_api_route("/data", self._data_page, methods=["GET"])
-        self.router.add_api_route("/visualization", self._visualization_page, methods=["GET"])
-        self.router.add_api_route("/settings", self._settings_page, methods=["GET"])
         
     async def _root(self, request: Request):
         
         self._logger.info("_root called")
         return self.templates.TemplateResponse("index.html", {"request": request})
-    
-    async def _data_page(self, request: Request):
-        self._logger.info("_data_page called")
-        return self.templates.TemplateResponse("data.html", {"request": request, "page_title": "Data Monitoring"})
-    
-    async def _visualization_page(self, request: Request):
-        self._logger.info("_visualization_page called")
-        return self.templates.TemplateResponse("visualization.html", {"request": request, "page_title": "Data Visualization"})
-    
-    async def _settings_page(self, request: Request):
-        self._logger.info("_settings_page called")
-        return self.templates.TemplateResponse("settings.html", {"request": request, "page_title": "Settings"})
